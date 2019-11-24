0deb468db6936a42c1f577cafbc86a041578b5b9
prorok.rv <prorok.rv@gmail.com>
2019-11-24 12:26:18 +0200

    UserInfo from db

---
+++ UserProfile.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    
+</head>
+<body>
+
+<?php 
+include_once "con_db.php";
+?>
+
+<div class="container">
+    <h1>User Info from DB</h1>
+
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">SurName</th>
+            <th scope="col">Age</th>
+            <th scope="col">Sex</th>
+            <th scope="col">Phone</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+        $sth = $dbh->prepare("SELECT Id, Name, SurName, Age, Sex, Phone FROM `tbl_users`");
+        $sth->execute();
+
+        while($result = $sth->fetch(PDO::FETCH_ASSOC))
+        {
+            echo '
+        <tr>
+            <th scope="row">'.$result["Id"].'</th>
+            <td>'.$result["Name"].'</td>
+            <td>'.$result["SurName"].'</td>
+            <td>'.$result["Age"].'</td>
+            <td>'.$result["Sex"].'</td>
+            <td>'.$result["Phone"].'</td>
+        </tr>
+        ';
+        }
+        ?>
+        </tbody>
+    </table>
+    
+</div>
+<script src="/js/jquery-3.4.1.min.js"></script>
+<script src="/js/popper.min.js"></script>
+<script src="/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_NAME','db_1.0');
+define('DB_USER','root');
+define('DB_PASSWORD','');
+define('DB_HOST','localhost');
+define('DB_CHARSET','utf8');
+define('DB_DRIVER','mysql');
